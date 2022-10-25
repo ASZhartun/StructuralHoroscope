f5313e1591ab3cfa1da843c8136ce80e342a01db
ASZhartun <as.zhartun@gmail.com>
2022-10-25 17:09:05 +0300

    feature: add fragment for result content representation

--- src/com/example/structuralhoroscope/MainActivity.java
+++ src/com/example/structuralhoroscope/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.webkit.WebView;
 
 public class MainActivity extends FragmentActivity {
 
@@ -12,6 +13,7 @@ public class MainActivity extends FragmentActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
 	}
 
 }
