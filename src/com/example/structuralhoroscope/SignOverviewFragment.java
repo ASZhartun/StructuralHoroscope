6b121534a12527eee64125301cd3a7ea59e8210b
ASZhartun <as.zhartun@gmail.com>
2022-10-25 15:34:10 +0300

    feature: add xml features for main activity with two fragments

---
+++ src/com/example/structuralhoroscope/SignOverviewFragment.java
@@ -0,0 +1,24 @@
+package com.example.structuralhoroscope;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class SignOverviewFragment extends Fragment {
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+	}
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		View v = (View) inflater.inflate(R.layout.fragment_overview, container);
+		return v;
+	}
+	
+}
