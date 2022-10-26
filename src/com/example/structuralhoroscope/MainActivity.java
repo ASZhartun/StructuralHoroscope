366436e77fd3770bc4b0135035f4e144b08b71f4
ASZhartun <as.zhartun@gmail.com>
2022-10-26 09:37:27 +0300

    feature: add date picker for choose button

--- src/com/example/structuralhoroscope/MainActivity.java
+++ src/com/example/structuralhoroscope/MainActivity.java
@@ -4,14 +4,19 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.TimeZone;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 
 public class MainActivity extends FragmentActivity {
 
 	final String DATE_PATTERN = "dd.MM.yyyy";
+	int mYear = 1990, mMonth = 0, mDay = 1;
 
 	EditText birthdayField;
 	Button chooseButton;
@@ -20,7 +25,7 @@ public class MainActivity extends FragmentActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		
+
 		birthdayField = (EditText) findViewById(R.id.BirthdayField);
 		chooseButton = (Button) findViewById(R.id.ChooseButton);
 
@@ -28,7 +33,32 @@ public class MainActivity extends FragmentActivity {
 		SimpleDateFormat sdf = new SimpleDateFormat(DATE_PATTERN);
 		sdf.setTimeZone(TimeZone.getDefault());
 		birthdayField.setText(sdf.format(date));
-		
+
+	}
+
+	public void onDatePicker(View v) {
+		DatePickerDialog dpd = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+			@Override
+			public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+				mDay = dayOfMonth;
+				mMonth = month;
+				mYear = year;
+				month++;
+				String day = String.valueOf(dayOfMonth);
+				String corrMonth = String.valueOf(month);
+				if (dayOfMonth < 10) {
+					day = "0" + dayOfMonth;
+				}
+				if (month < 10) {
+					corrMonth = "0" + month;
+				}
+
+				StringBuilder sb = new StringBuilder();
+				sb.append(day).append(".").append(corrMonth).append(".").append(year);
+				birthdayField.setText(sb.toString());
+			}
+		}, mYear, mMonth, mDay);
+		dpd.show();
 	}
 
 }
